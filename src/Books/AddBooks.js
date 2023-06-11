b4f21819881012fc13b5901368b4ebc316f8891f
Aniket Bose <aniketbose986@gmail.com>
2023-06-11 21:34:23 +0530

    AddUser works now

--- src/Books/AddBooks.js
+++ src/Books/AddBooks.js
@@ -1,6 +1,8 @@
 import Button from "../signIn/Button";
 import styles from '../signIn/Button.module.css';
-
+import React from "react";
+import Navbar from "../Navbar/Navbar";
+import ReactDOM from "react-dom";
 const AddBooks = () => {
     const formSubmissionHandler = (event) => {
         event.preventDefault();
@@ -14,17 +16,20 @@ const AddBooks = () => {
         }).then(res => res.json()).then(console.log);
     }
     return (
-        <form className="row row-cols-lg-auto g-3 align-items-center" onSubmit={formSubmissionHandler}>
-            <div className="col-md-3">
-                <label className="visually-hidden" htmlFor="inputName" />
-                <div className="input-group">
-                    <input type="text" className="form-control" id="inputName" placeholder="Book name" required />
+        <React.Fragment>
+            {ReactDOM.createPortal(<Navbar />, document.getElementById('navbar'))}
+            <form className="row row-cols-lg-auto g-3 align-items-center" onSubmit={formSubmissionHandler}>
+                <div className="col-md-3">
+                    <label className="visually-hidden" htmlFor="inputName" />
+                    <div className="input-group">
+                        <input type="text" className="form-control" id="inputName" placeholder="Book name" required />
+                    </div>
                 </div>
-            </div>
-            <div className="col-md-3">
-                <Button value="Add Book" style={styles.customBtnPrimary} />
-            </div>
-        </form>
+                <div className="col-md-3">
+                    <Button value="Add Book" style={styles.customBtnPrimary} />
+                </div>
+            </form>
+        </React.Fragment>
     );
 }
 
--- src/Home/AddUser.js
+++ src/Home/AddUser.js
@@ -1,46 +1,76 @@
 import Button from "../signIn/Button";
 import { useState } from "react";
 import styles from '../signIn/Button.module.css';
+import Navbar from "../Navbar/Navbar";
+import ReactDOM from "react-dom";
+import React from "react";
 const AddUser = () => {
     const [inPass, setInPass] = useState('');
-    const [inPassCheck, setInpPassCheck]= useState(true);
+    const [inPassCheck, setInpPassCheck] = useState(true);
+    const [input, setInput]=useState({});
 
-    const passChangeHandler=(event) => {
+    const passChangeHandler = (event) => {
         setInPass(event.target.value);
     }
-    
+
+    const changeHandler = () => {
+        passChangeHandler();
+        handleChange();
+    }
+
+    const handleChange = (event) => {
+        const name=event.target.name;
+    }
+
     const formSubmissionHandler = (event) => {
         event.preventDefault();
         if (inPass.trim() === '' || inPass.length <= 6)
             setInpPassCheck(false);
-        else
+        else {
             setInpPassCheck(true);
+            console.log(event.target.value);
+            // let url;
+            // url = `http://localhost:8081/user/create/`;
+            // fetch(url, {
+            //     method: 'POST',
+            //     body: newData,
+            //     headers: {
+            //         'Content-type': 'application/json'
+            //     }
+            // }).then(res => res.json()).then(console.log);
+        }
     }
     const passStyle = inPassCheck ? `form-control` : `form-control is-invalid`;
     return (
-        <form className="row row-cols-lg-auto g-3 align-items-center" onSubmit={formSubmissionHandler}>
-            <div className="col-md-3">
-                <label className="visually-hidden" htmlFor="inputName" />
-                <div className="input-group">
-                    <input type="text" className="form-control" id="inputName" placeholder="Fullname" required/>
+        <React.Fragment>
+            {ReactDOM.createPortal(<Navbar />, document.getElementById('navbar'))}
+            <form className="" onSubmit={formSubmissionHandler} onChange={handleChange}>
+                <div className="form-group">
+                    <label className="visually-hidden" htmlFor="inputName" />
+                    <div className="input-group">
+                        <input type="text" className="form-control" id="inputName" placeholder="Fullname" required onChange={handleChange}/>
+                    </div>
                 </div>
-            </div>
-            <div className="col-md-3">
-                <label className="visually-hidden" htmlFor="inputUser" />
-                <div className="input-group">
-                    <input type="text" className="form-control" id="inputUser" placeholder="Username" required/>
+                <br />
+                <div className="form-group">
+                    <label className="visually-hidden" htmlFor="inputUser" />
+                    <div className="input-group">
+                        <input type="text" className="form-control" id="inputUser" placeholder="Username" required onChange={handleChange}/>
+                    </div>
                 </div>
-            </div>
-            <div className="col-md-3">
-                <label className="visually-hidden" htmlFor="inputPass" />
-                <div className="input-group">
-                    <input type="password" className={passStyle} id="inputPass" placeholder="Password" onChange={passChangeHandler}/>
+                <br />
+                <div className="form-group">
+                    <label className="visually-hidden" htmlFor="inputPass" />
+                    <div className="input-group">
+                        <input type="password" className={passStyle} id="inputPass" placeholder="Password" onChange={changeHandler} />
+                    </div>
                 </div>
-            </div>
-            <div className="col-md-3">
-                <Button value="Add User" style={styles.customBtnPrimary}/>
-            </div>
-        </form>
+                <br />
+                <div className="form-group">
+                    <Button value="Add User" style={styles.customBtnPrimary} />
+                </div>
+            </form>
+        </React.Fragment>
     );
 }
 
